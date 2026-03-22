a6b9c293fe0b62f8c84c610dd4d59b922fdf6074
Adam Rodwell <adam.richard.rodwell@gmail.com>
2026-03-22 11:12:52 +1100

    working on a utils lib

---
+++ src/libs/Utils/src/UtilsComponent.cpp
@@ -0,0 +1,51 @@
+#include "UtilsComponent.hpp"
+#include "Arp/Plc/Commons/Esm/ProgramComponentBase.hpp"
+#include "UtilsLibrary.hpp"
+
+namespace Utils
+{
+
+UtilsComponent::UtilsComponent(IApplication& application, const String& name)
+: ComponentBase(application, ::Utils::UtilsLibrary::GetInstance(), name, ComponentCategory::Custom)
+, programProvider(*this)
+, ProgramComponentBase(::Utils::UtilsLibrary::GetInstance().GetNamespace(), programProvider)
+{
+}
+
+void UtilsComponent::Initialize()
+{
+    // never remove next line
+    ProgramComponentBase::Initialize();
+
+    // subscribe events from the event system (Nm) here
+}
+
+void UtilsComponent::LoadConfig()
+{
+    // load project config here
+}
+
+void UtilsComponent::SetupConfig()
+{
+    // never remove next line
+    ProgramComponentBase::SetupConfig();
+
+    // setup project config here
+}
+
+void UtilsComponent::ResetConfig()
+{
+    // never remove next line
+    ProgramComponentBase::ResetConfig();
+
+    // implement this inverse to SetupConfig() and LoadConfig()
+}
+
+void UtilsComponent::PowerDown()
+{
+	// implement this only if data must be retained even on power down event
+	// will work only for PLCnext controllers with an "Integrated uninterruptible power supply (UPS)"
+	// Available with 2021.6 FW
+}
+
+} // end of namespace Utils
---
+++ src/libs/Utils/src/UtilsProgram.cpp
@@ -0,0 +1,13 @@
+#include "UtilsProgram.hpp"
+#include "Arp/System/Commons/Logging.h"
+#include "Arp/System/Core/ByteConverter.hpp"
+
+namespace Utils
+{
+ 
+void UtilsProgram::Execute()
+{
+    //implement program 
+}
+
+} // end of namespace Utils
